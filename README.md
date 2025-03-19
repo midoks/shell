2c4bbec21cec64052640b7483caf19a91f6374f9
dami <midoks@163.com>
2025-03-19 12:51:40 +0800

    update

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # shell
 
 各种操作和维护脚本
+
+### tcp_syn_flood (tcp洪水防护)
+
+-  安装
+```
+curl -fsSL https://raw.githubusercontent.com/midoks/shell/refs/heads/main/tcp_syn_flood/install.sh| sh
+```
+
+
+
--- tcp_syn_flood/install.sh
+++ tcp_syn_flood/install.sh
@@ -2,7 +2,8 @@
 
 
 echo "install mtsf start"
-if [ -f /usr/bin/mtsf ];then
+if [ ! -f /usr/bin/mtsf ];then
 	wget -0 /usr/bin/mtsf https://raw.githubusercontent.com/midoks/shell/refs/heads/main/tcp_syn_flood/mtsf.sh
 fi
 
+echo "install mtsf end"
