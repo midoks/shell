93dcbf5a35fa083e3509a0a6602cf4d4a012d380
dami <midoks@163.com>
2025-03-19 12:57:25 +0800

    Update install.sh

--- tcp_syn_flood/install.sh
+++ tcp_syn_flood/install.sh
@@ -6,6 +6,7 @@ which
 
 if [ ! -f /usr/bin/mtsf ];then
 	wget -O /usr/bin/mtsf https://raw.githubusercontent.com/midoks/shell/refs/heads/main/tcp_syn_flood/mtsf.sh
+	chmod +x /usr/bin/mtsf
 fi
 
 echo "install mtsf end"
