f699d05cc48965e7bb7b353b57446f8e5d5c9a4b
dami <midoks@163.com>
2025-03-20 23:32:03 +0800

    Update install.sh

--- tcp_syn_flood/install.sh
+++ tcp_syn_flood/install.sh
@@ -10,6 +10,7 @@ which apt && apt install -y net-tools
 which apt && apt install -y conntrack
 which apt && apt install -y geoip-bin
 which apt && apt install -y at
+which apt && apt install -y iptables
 
 if [ ! -f /usr/bin/mtsf ];then
 	wget -O /usr/bin/mtsf https://raw.githubusercontent.com/midoks/shell/refs/heads/main/tcp_syn_flood/mtsf.sh?t=${TIME}
