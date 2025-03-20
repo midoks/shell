4ee0bec68fd9733033e73cf489896ab7eb38f1df
dami <midoks@163.com>
2025-03-20 19:52:36 +0800

    Update mtsf.sh

--- tcp_syn_flood/mtsf.sh
+++ tcp_syn_flood/mtsf.sh
@@ -27,6 +27,9 @@ RUN_CMD(){
 	# 获取所有 SYN_RECV 状态的连接
 	netstat -an | grep SYN_RECV | while read line; do
 		echo $line
+		SRC_IP=$(echo "$line" | awk '{print $5}' | cut -d= -f2)
+
+		echo "SRC_IP:$SRC_IP"
 		# if [[ "$line" =~ "conntrack-tools" ]];then
 		# 	echo $line
 		# 	continue
