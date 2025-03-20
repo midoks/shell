a7d3fc3508540a3ea4f77fa6b064a8c0bf61714a
dami <midoks@163.com>
2025-03-21 01:14:00 +0800

    update

--- tcp_syn_flood/mtsf.bk1.sh
+++ tcp_syn_flood/mtsf.bk1.sh
@@ -84,7 +84,6 @@ RUN_CMD(){
 
 	# 获取所有 SYN_RECV 状态的连接
 	netstat -an | grep SYN_RECV | while read line; do
-	netstat -an | grep tcp | while read line; do
 		# echo $line
 		SRC_IP=$(echo "$line" | awk '{print $5}' | cut -d= -f2)
 		# echo "SRC_IP:$SRC_IP"
@@ -116,9 +115,54 @@ RUN_CMD(){
 				MF_BAN_DO $SUBNET_IP
 			fi
 
+			# FIND_SUBNET_IP=`iptables -L -n | grep $SUBNET_IP`
+			# if [[ "$FIND_SUBNET_IP" == "" ]];then
+			# 	echo "IP $SUBNET_IP 来自 $COUNTRY，将被封禁5分钟。"
+			#     # 封禁IP地址
+			#     echo "iptables -A INPUT -s $SUBNET_IP -j DROP"
+			#     iptables -A INPUT -s $SUBNET_IP -j DROP
+			#     # 5分钟后解封
+			#     echo "iptables -D INPUT -s $SUBNET_IP -j DROP" | at now + 5 minutes
+			# else
+			# 	echo "IP $SUBNET_IP 来自 $COUNTRY，已经封禁。"
+			# fi
 		else
 		    echo "IP $IP 来自 $COUNTRY，允许访问。"
 		fi
+
+		# if [[ "$line" =~ "conntrack-tools" ]];then
+		# 	echo $line
+		# 	continue
+		# fi
+
+	    # # 提取连接信息
+	    # SRC_IP=$(echo $line | awk '{print $5}' | cut -d= -f2)
+	    # DST_IP=$(echo $line | awk '{print $6}' | cut -d= -f2)
+	    # SRC_PORT=$(echo $line | awk '{print $8}' | cut -d= -f2)
+	    # DST_PORT=$(echo $line | awk '{print $9}' | cut -d= -f2)
+	    # echo "line:$line"
+	    # echo "SRC_IP:$SRC_IP,DST_IP:$DST_IP,SRC_PORT:$SRC_PORT,DST_PORT:$DST_PORT"
+
+	    # # 获取连接的创建时间
+	    # CREATED=$(echo $line | grep -oP 'start=\K[0-9]+')
+
+	    # # 计算连接持续时间
+	    # DURATION=$((CURRENT_TIME - CREATED))
+
+	    # echo "DURATION:$DURATION"
+
+	    # # 如果连接持续时间超过超时时间，则禁止该IP
+	    # if [ $DURATION -ge $TIMEOUT ]; then
+	    #     echo "Banning $SRC_IP for $BAN_TIME seconds due to SYN_RECV timeout."
+	    #     iptables -A INPUT -s $SRC_IP -j DROP
+
+	    #     # 设置一个定时任务，5分钟后解禁该IP
+	    #     (
+	    #         sleep $BAN_TIME
+	    #         iptables -D INPUT -s $SRC_IP -j DROP
+	    #         echo "Unbanned $SRC_IP after $BAN_TIME seconds."
+	    #     ) &
+	    # fi
 	done
 }
 
