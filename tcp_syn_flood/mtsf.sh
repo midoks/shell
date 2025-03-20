bd66f05e77fd1c2c20986f7b069c0614a17d4262
dami <midoks@163.com>
2025-03-20 16:23:21 +0800

    Update mtsf.sh

--- tcp_syn_flood/mtsf.sh
+++ tcp_syn_flood/mtsf.sh
@@ -140,10 +140,9 @@ MF_CONF_OPT(){
 	echo "===== TCP参数优化完成 ====="
 
 
-
-
 	# echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
 	# echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
+	sysctl -p
 
 	echo -e "done!"
 }
