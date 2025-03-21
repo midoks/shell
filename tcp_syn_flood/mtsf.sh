ad0a125f5cde65f7d8034d4441a5a37eec67e46c
dami <midoks@163.com>
2025-03-21 18:49:02 +0800

    Update mtsf.sh

--- tcp_syn_flood/mtsf.sh
+++ tcp_syn_flood/mtsf.sh
@@ -527,8 +527,9 @@ MF_CONF_NET_MBPS(){
 	TX_BW_MBPS=$((TX_BW * 8 / 1000000))
 
 	# 总带宽使用率
-	TOTAL_BW_MBPS=$((RX_BW_MBPS + TX_BW_MBPS))
-	echo "当前使用带宽使用率:${TOTAL_BW_MBPS}"
+	BYTES_T=$((TX_BW + TX_BW))
+	TOTAL_BW_MBPS=$((RX_BW_MBPS + TX_BW_MBPS)/1000000)
+	echo "当前使用带宽使用率:$BYTES_T-${TOTAL_BW_MBPS}"
 }
 
 MF_TCP_INFO(){
