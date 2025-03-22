7aeb3873e753f4acbdb67da5c0b1a1105da41251
dami <midoks@163.com>
2025-03-22 16:24:05 +0800

    Update mtsf.sh

--- tcp_syn_flood/mtsf.sh
+++ tcp_syn_flood/mtsf.sh
@@ -525,6 +525,14 @@ MF_CONF_OPT(){
 	ifconfig ${NET_ETH} mtu 9000
 	echo "===启用巨帧（Jumbo Frames）[ifconfig ${NET_ETH} mtu 9000]==="
 
+	echo "===当前接口的LRO状态[ethtool -k ${NET_ETH} | grep large-receive-offload]==="
+
+	ethtool -K ${NET_ETH} gro on
+	echo "===设置接口队列长度[ethtool -K ${NET_ETH} gro on]==="
+
+	ethtool -K ${NET_ETH} gro on
+	echo "===设置接口队列长度[ethtool -K ${NET_ETH} lro on]==="
+
 	ethtool -G ${NET_ETH} rx 4096 tx 4096
 	echo "===设置接口队列长度[thtool -G ${NET_ETH} rx 4096 tx 4096]==="
 	
