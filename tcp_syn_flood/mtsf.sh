79d02b9d938b40f09aa097dac79f042fadab7c0d
dami <midoks@163.com>
2025-03-30 00:44:09 +0800

    Update mtsf.sh

--- tcp_syn_flood/mtsf.sh
+++ tcp_syn_flood/mtsf.sh
@@ -685,7 +685,7 @@ MF_TCP_RERADIO2(){
 		else
 			status="极差*必须排查（防火墙、带宽、路由）"
 		fi
-	    echo "当前TCP重传率: ${retrans_rate}% | 发送段: $delta_sent | 重传: $delta_sent - ${status}"
+	    echo "当前TCP重传率: ${retrans_rate}% | 发送段: ${delta_sent}/s | 重传: ${delta_sent}/s - ${status}"
 	else
 	    echo "当前TCP重传率: 未发送数据，无法计算重传率。"
 	fi
