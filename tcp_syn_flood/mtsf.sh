0e7da4e443c50380089fb9a75d13d467d61e6547
dami <midoks@163.com>
2025-03-30 00:19:52 +0800

    Update mtsf.sh

--- tcp_syn_flood/mtsf.sh
+++ tcp_syn_flood/mtsf.sh
@@ -640,7 +640,7 @@ MF_TCP_RERADIO(){
 	sent=$(netstat -s | awk '/segments sent out/ {print $1}')
 	retrans=$(netstat -s | awk '/segments retransmitted/ {print $1}')
 	if [ "$sent" -gt 0 ]; then
-        retrans_rate=$(echo "scale=2; $retrans / $sent * 100" | bc)
+        retrans_rate=$(echo "scale=4; $retrans / $sent * 100" | bc)
 	    echo "当前TCP重传率: ${retrans_rate}% | 发送段: $sent | 重传: $retrans"
 	else
 	    echo "当前TCP重传率: 未发送数据，无法计算重传率。"
