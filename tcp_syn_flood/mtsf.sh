118ac19edac14dd4e5bc4175c27bd61dbf778efc
dami <midoks@163.com>
2025-04-03 02:29:50 +0800

    Update mtsf.sh

--- tcp_syn_flood/mtsf.sh
+++ tcp_syn_flood/mtsf.sh
@@ -571,7 +571,7 @@ MF_CONF_OPT(){
 	FIND_NI_tcp_keepalive_time=`cat /etc/sysctl.conf | grep net.ipv4.tcp_keepalive_time`
 	if [ "$FIND_NI_tcp_keepalive_time" == "" ];then
 		echo 180 > /proc/sys/net/ipv4/tcp_keepalive_time
-		echo "net.ipv4.tcp_keepalive_time = 180" >> /etc/sysctl.conf
+		echo "net.ipv4.tcp_keepalive_time = 300" >> /etc/sysctl.conf
 	else
 		echo "net.ipv4.tcp_keepalive_time exist!"
 	fi
